3801264bfcf6871316af86b9e8a12dec9960b41a
Milan1302 <103596609+Milan1302@users.noreply.github.com>
2023-11-26 16:47:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -143,3 +143,12 @@ Blogs = document.getElementById("Blogs");
 Blogs.addEventListener('click', function() {
   scrollTo(0, (4*windowheight/0.99));
 })
+
+// For animation on projects page
+$(".Click-here").on('click', function() {
+  $(".custom-model-main").addClass('model-open');
+}); 
+$(".close-btn, .bg-overlay").click(function(){
+  $(".custom-model-main").removeClass('model-open');
+});
+
